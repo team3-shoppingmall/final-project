360d4a463d46b84be2f41af4e82738ec80bcb05c
portal116 <tim950116@gmail.com>
2022-02-17 23:17:39 +0900

    'kang'

--- src/main/java/com/myspring/spring/faq/FaqMapper.java
+++ src/main/java/com/myspring/spring/faq/FaqMapper.java
@@ -4,10 +4,12 @@ import java.util.List;
 
 import org.apache.ibatis.annotations.Delete;
 import org.apache.ibatis.annotations.Insert;
+import org.apache.ibatis.annotations.Mapper;
 import org.apache.ibatis.annotations.Param;
 import org.apache.ibatis.annotations.Select;
 import org.apache.ibatis.annotations.Update;
 
+@Mapper
 public interface FaqMapper {
     @Select("select * from faq")
 	List<FaqVO> getFaqAll();

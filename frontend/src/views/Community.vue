24a45e8689f50937dbc968d3aef1de56b49a99ea
portal116 <tim950116@gmail.com>
2022-02-17 16:37:28 +0900

    'kang'

--- frontend/src/views/Community.vue
+++ frontend/src/views/Community.vue
@@ -1,16 +1,47 @@
 <template>
 <v-container>
-    Community Page
-    <v-btn :to="'/community/notice'">notice</v-btn>
-    <v-btn :to="'/community/review'">review</v-btn>
-    <v-btn :to="'/community/faq'">FAQ</v-btn>
+    <h3>{{UPPER(pagename)}}</h3>
+    <v-row justify="center">
+        <v-col cols="auto">
+            <v-btn :to="'/community/notice'">notice</v-btn>
+        </v-col>
+        <v-col cols="auto">
+            <v-btn :to="'/community/review'">review</v-btn>
+        </v-col>
+        <v-col cols="auto">
+            <v-btn :to="'/community/faq'">FAQ</v-btn>
+        </v-col>
+    </v-row>
     <router-view></router-view>
 </v-container>
 </template>
 
 <script>
 export default {
-
+    data() {
+        return {
+            pagename: '',
+        }
+    },
+    methods: {
+        currentURL() {
+            let link = document.location.href;
+            link = link.substring(32, link.length);
+            this.pagename = link;
+        },
+        UPPER(word) {
+            return word.toUpperCase();
+        }
+    },
+    computed: {},
+    watch: {
+        '$route'() {
+            this.currentURL();
+        }
+    },
+    mounted() {
+        this.currentURL();
+    }
 }
 </script>
 
--- frontend/src/views/Notice.vue
+++ frontend/src/views/Notice.vue
@@ -1,12 +1,36 @@
 <template>
 <v-container>
-    Notice Page
+    <v-data-table :headers="headers" :items="contents" :items-per-page="5" class="elevation-1"></v-data-table>
 </v-container>
 </template>
 
 <script>
 export default {
-
+    data() {
+        return {
+            headers: [{
+                    text: '번호',
+                    align: 'start',
+                    sortable: false,
+                    value: 'noticeno',
+                },
+                {
+                    text: '제목',
+                    value: 'title'
+                },
+                {
+                    text: '작성자',
+                    value: 'ID'
+                },
+            ],
+            contents: [{
+                    noticeno: '16',
+                    title: '2022년 02월 07일 이전 개인정보 처리방침',
+                    ID: '슬로우엔드',
+                },
+            ],
+        }
+    },
 }
 </script>
 

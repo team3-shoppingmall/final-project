2be6851fdb687181816e77518f16505f46f30a48
sunshine4437 <sunshine4437@gmail.com>
2022-02-20 20:57:16 +0900

    1

--- frontend/src/views/Community/Review.vue
+++ frontend/src/views/Community/Review.vue
@@ -1,11 +1,32 @@
 <template>
 <v-container>
     <div>
-        <v-data-table :headers="headers" :options.sync="options" :items="contents" :server-items-length="totalContents" :loading="loading" class="elevation-1"></v-data-table>
+        <v-data-table :headers="headers" :options.sync="options" :items="contents" :server-items-length="totalContents" :loading="loading" class="elevation-1" disable-sort>
+            <template v-slot:body="{ items }">
+                <tbody>
+                    <tr v-for="item in items" :key="item.reviewNo">
+                        <td style="text-align: center;">{{ item.reviewNo }}</td>
+                        <td>
+                            <productNameDisplay :productno="item.productno" />
+                        </td>
+                        <td style="text-align: center;" class="pa-1">
+                            <v-rating hover length="5" readonly size="10" :value="item.star"></v-rating>
+                        </td>
+                        <td>{{ item.content }}</td>
+                        <td>
+                            <hideId :id="item.id" />
+                        </td>
+                        <td style="text-align: center;">
+                            <dateDisplay :regDate="item.regDate" />
+                        </td>
+                    </tr>
+                </tbody>
+            </template>
+        </v-data-table>
     </div>
 
     <v-row align="center" justify="space-between">
-        <v-col class="d-flex" cols="8" sm="7" md="5" lg="4" xl="3">
+        <v-col class="d-flex" cols="8" sm="7" md="6" lg="5" xl="4">
             <v-row>
                 <v-col cols="4">
                     <v-select :items="searches" v-model="search"></v-select>
@@ -24,7 +45,15 @@
 
 <script>
 import axios from 'axios'
+import hideId from '@/components/hideId.vue'
+import dateDisplay from '@/components/dateDisplay.vue'
+import productNameDisplay from '@/components/productNameDisplay.vue'
 export default {
+    components: {
+        hideId,
+        dateDisplay,
+        productNameDisplay,
+    },
     data() {
         return {
             totalContents: 0,
@@ -34,39 +63,46 @@ export default {
             headers: [{
                     text: '번호',
                     value: 'reviewno',
-                    sortable: false,
-                    width: '10%'
+                    width: '10%',
+                    align: 'center',
+                    divider: true
                 },
                 {
                     text: '상품명',
-                    value: 'productname',
-                    sortable: false,
-                    width: '10%'
+                    value: 'productno',
+                    width: '10%',
+                    align: 'center',
+                    divider: true
                 },
                 {
                     text: '별점',
                     value: 'star',
-                    sortable: false,
-                    width: '10%'
+                    width: '10%',
+                    align: 'center',
+                    divider: true
                 },
                 {
                     text: '후기',
                     value: 'content',
-                    sortable: false,
-                    width: '60%'
+                    width: '45%',
+                    align: 'center',
+                    divider: true
                 },
                 {
                     text: '작성자',
                     value: 'id',
-                    sortable: false,
-                    width: '10%'
+                    width: '10%',
+                    align: 'center',
+                    divider: true
+                },
+                {
+                    text: '작성일',
+                    value: 'regDate',
+                    width: '10%',
+                    align: 'center'
                 },
             ],
             searches: [{
-                    text: '제목',
-                    value: 'title'
-                },
-                {
                     text: '상품명',
                     value: 'productname'
                 },
@@ -75,7 +111,7 @@ export default {
                     value: 'id'
                 }
             ],
-            search: 'title',
+            search: 'id',
             searchWord: '',
         }
     },
@@ -88,7 +124,7 @@ export default {
             } = this.options
             axios({
                     method: 'get',
-                    url: `/api/notice/getNotice`,
+                    url: `/api/review/getReview`,
                     params: {
                         page: page,
                         perPage: itemsPerPage,
@@ -101,7 +137,7 @@ export default {
                     this.loading = false
                     axios({
                             method: 'get',
-                            url: '/api/notice/getCount',
+                            url: '/api/review/getCount',
                             params: {
                                 search: this.search,
                                 searchWord: this.searchWord,
@@ -124,6 +160,12 @@ export default {
 }
 </script>
 
-<style>
+<style scoped>
+table td {
+    border-right: 1px solid #dddddd;
+}
 
+table td:last-child {
+    border-right: none
+}
 </style>

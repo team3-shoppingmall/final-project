3ec2b87a662eb81494157385ca27cc11c25baa10
portal116 <tim950116@gmail.com>
2022-02-21 18:37:41 +0900

    'kang'

--- Database/review.sql
+++ Database/review.sql
@@ -1,13 +1,12 @@
-create database springdb;
 use springdb;
 drop table if exists reviewtable;
 
-create table ReviewTable(
+create table reviewTable(
 	reviewNo bigint not null auto_increment,	-- 후기번호(자동증가)
 	-- productNo int not null,				-- 상품번호 외래키
     content varchar(600) not null,          -- 내용
 	id varchar(50) not null,                -- 등록아이디
-    regDate date default (current_timestamp),    -- 등록날짜
+    regDate timestamp default (current_timestamp),    -- 등록날짜
 	image varchar(100),                     -- 사진이름
 	star int not null,                      -- 별점
 
@@ -28,8 +27,8 @@ insert into reviewtable(content, id, image, star) values('후기내용5','user5'
 
 commit;
 
-select * from reviewtable
+select * from reviewtable;
 
-select * from reviewtable order by reviewno desc limit 3,10;
-select * from reviewtable order by reviewno desc;
-select * from reviewtable where id like '%%' order by reviewno desc limit 1,5;
+-- select * from reviewtable order by reviewno desc limit 3,10;
+-- select * from reviewtable order by reviewno desc;
+-- select * from reviewtable where id like '%%' order by reviewno desc limit 1,5;

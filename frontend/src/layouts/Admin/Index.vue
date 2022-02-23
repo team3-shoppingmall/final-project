9a1ca0e5e298510c5e22125e3bde0edb8cb0efd4
sunshine4437 <sunshine4437@gmail.com>
2022-02-23 16:59:20 +0900

    go

--- frontend/src/layouts/Admin/Index.vue
+++ frontend/src/layouts/Admin/Index.vue
@@ -25,13 +25,13 @@
         <v-divider></v-divider>
 
         <v-list dense nav>
-            <v-list-item v-for="item in items" :key="item.title" link :to="item.to" active-class="primary white--text" >
+            <v-list-item v-for="item in items" :key="item.title" link :to="item.to" active-class="primary white--text">
                 <v-list-item-icon>
                     <v-icon>{{ item.icon }}</v-icon>
                 </v-list-item-icon>
 
                 <v-list-item-content>
-                    <v-list-item-title class="text-h6">{{ item.title }}</v-list-item-title>
+                    <v-list-item-title>{{ item.title }}</v-list-item-title>
                 </v-list-item-content>
             </v-list-item>
         </v-list>

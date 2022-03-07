aabaafecfcfbd7c8c671ccdb0f9aafb73d1bd811
portal116 <tim950116@gmail.com>
2022-03-07 11:19:40 +0900

    'kang

--- frontend/src/layouts/Default/Index.vue
+++ frontend/src/layouts/Default/Index.vue
@@ -8,15 +8,37 @@
                     <v-col cols="auto">
                         <v-menu open-on-hover bottom offset-y v-for="(page ,idx) in pages" :key="idx">
                             <template v-slot:activator="{ on, attrs }">
-                                <v-btn color="primary" dark v-bind="attrs" v-on="on" :to="page.to" v-if="page.name != 'Community'">
-                                    {{page.name}}
-                                </v-btn>
-                                <v-btn color="primary" dark v-bind="attrs" v-on="on" v-if="page.name == 'Community'">
+                                <v-btn color="primary" dark v-bind="attrs" v-on="on" :to="page.to">
                                     {{page.name}}
                                 </v-btn>
                             </template>
 
                             <v-list v-if="page.name=='Community'">
+                                <v-list-item v-for="(item, index) in page.items" :key="index" :to="item.to">
+                                    <v-list-item-title>{{ item.title }}</v-list-item-title>
+                                </v-list-item>
+                            </v-list>
+                            <v-list v-if="page.name=='OUTER'">
+                                <v-list-item v-for="(item, index) in page.items" :key="index" :to="item.to">
+                                    <v-list-item-title>{{ item.title }}</v-list-item-title>
+                                </v-list-item>
+                            </v-list>
+                            <v-list v-if="page.name=='TOP'">
+                                <v-list-item v-for="(item, index) in page.items" :key="index" :to="item.to">
+                                    <v-list-item-title>{{ item.title }}</v-list-item-title>
+                                </v-list-item>
+                            </v-list>
+                            <v-list v-if="page.name=='PANTS'">
+                                <v-list-item v-for="(item, index) in page.items" :key="index" :to="item.to">
+                                    <v-list-item-title>{{ item.title }}</v-list-item-title>
+                                </v-list-item>
+                            </v-list>
+                            <v-list v-if="page.name=='SKIRT'">
+                                <v-list-item v-for="(item, index) in page.items" :key="index" :to="item.to">
+                                    <v-list-item-title>{{ item.title }}</v-list-item-title>
+                                </v-list-item>
+                            </v-list>
+                            <v-list v-if="page.name=='DRESS'">
                                 <v-list-item v-for="(item, index) in page.items" :key="index" :to="item.to">
                                     <v-list-item-title>{{ item.title }}</v-list-item-title>
                                 </v-list-item>
@@ -68,6 +90,7 @@ export default {
                 to: '/basket',
             }, {
                 name: 'Community',
+                to: '/community/notice',
                 items: [{
                         title: 'NOTICE',
                         to: '/community/notice',
@@ -107,14 +130,82 @@ export default {
                 name: 'ProductDetail',
                 to: '/productDetail/1',
             }, {
-                name: 'ProductList',
-                to: '/productList/skirt/mini',
+                name: 'OUTER',
+                to: '/productList/outer/all',
+                items: [{
+                    title: '재킷',
+                    to: '/productList/outer/jacket',
+                }, {
+                    title: '가디건',
+                    to: '/productList/outer/cardigan',
+                }, {
+                    title: '점퍼',
+                    to: '/productList/outer/jumper',
+                }, {
+                    title: '코트',
+                    to: '/productList/outer/coat',
+                }, ],
+            }, {
+                name: 'TOP',
+                to: '/productList/top/all',
+                items: [{
+                    title: '슬리브리스',
+                    to: '/productList/top/sleeveless',
+                }, {
+                    title: '티셔츠',
+                    to: '/productList/top/tshirts',
+                }, {
+                    title: '니트',
+                    to: '/productList/top/knit',
+                }, {
+                    title: '셔츠',
+                    to: '/productList/top/shirt',
+                }, {
+                    title: '블라우스',
+                    to: '/productList/top/blouse',
+                }, {
+                    title: '맨투맨',
+                    to: '/productList/top/mtm',
+                }, ],
+            }, {
+                name: 'PANTS',
+                to: '/productList/pants/all',
+                items: [{
+                    title: '슬랙스',
+                    to: '/productList/pants/slacks',
+                }, {
+                    title: '데님',
+                    to: '/productList/pants/denim',
+                }, {
+                    title: '면',
+                    to: '/productList/pants/cotton',
+                }, {
+                    title: 'SHORT',
+                    to: '/productList/pants/shorts',
+                }, ],
+            }, {
+                name: 'SKIRT',
+                to: '/productList/skirt/all',
+                items: [{
+                    title: '미니',
+                    to: '/productList/skirt/mini',
+                }, {
+                    title: '미디/롱',
+                    to: '/productList/skirt/midi-long',
+                }, ],
+            }, {
+                name: 'DRESS',
+                to: '/productList/dress/all',
+                items: [{
+                    title: '원피스',
+                    to: '/productList/dress/ops',
+                }, {
+                    title: '점프수트',
+                    to: '/productList/dress/jumpSuit',
+                }, ],
             }, {
                 name: 'ProductSearch',
                 to: '/productSearch',
-            }, {
-                name: 'WishList',
-                to: '/wishList',
             }, {
                 name: 'Sign In',
                 to: '/authentication/signIn',
